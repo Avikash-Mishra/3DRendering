03969619735dca9c07c8174d8322a853c3b1840c
Avikash-Mishra <avimish62@gmail.com>
2013-08-14 19:33:49 +1000

    First push. Implemented simple GUI

---
+++ src/GUI.java
@@ -0,0 +1,63 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class GUI {
+	private JFrame WindowFrame;
+	private JComponent Drawing;
+	private JPanel TopPanel;
+	private int FrameWidth = 800;
+	private int FrameHeight = 800;
+	
+	public static void main(String[] arg){
+		GUI g = new GUI();
+	}
+	public GUI(){
+		setupFrame();
+	}
+
+
+	private void setupFrame() {
+		JButton quitButton = new JButton("Quit");
+		WindowFrame = new JFrame("3D Rendering");
+		WindowFrame.setSize(FrameWidth, FrameHeight);
+		WindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		WindowFrame.setResizable(true);
+		Drawing = new JPanel() {
+			protected void paintComponent(Graphics g){
+				super.paintComponent(g);
+				draw(g);
+			}
+		};
+		TopPanel = new JPanel();
+		TopPanel.add(quitButton);
+		quitButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(1);
+			}
+		});
+		Drawing.setSize(FrameWidth,FrameHeight);
+		Drawing.setBackground(Color.white);
+		WindowFrame.add(Drawing);
+		WindowFrame.add(TopPanel, BorderLayout.NORTH);
+		
+		WindowFrame.setVisible(true);
+	}
+	
+	private void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.CYAN);
+		g.drawRect(10, 10, 100, 100);
+	}
+}

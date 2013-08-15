b398042aacc8e779c89a194a6c56c914f3b68523
Avikash-Mishra <avimish62@gmail.com>
2013-08-15 23:25:17 +1200

    Implemented model class

---
+++ src/ModelObject.java
@@ -0,0 +1,36 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class ModelObject {
+	private ArrayList<Polygon> polygons;
+
+	/**
+	 * @param polygons
+	 */
+	public ModelObject(ArrayList<Polygon> polygons) {
+		super();
+		this.polygons = polygons;
+	}
+
+	/**
+	 * @return the polygons
+	 */
+	public ArrayList<Polygon> getPolygons() {
+		return polygons;
+	}
+
+	/**
+	 * @param polygons
+	 *            the polygons to set
+	 */
+	public void setPolygons(ArrayList<Polygon> polygons) {
+		this.polygons = polygons;
+	}
+
+	public void draw(Graphics g){
+		for(Polygon p : polygons){
+			p.draw(g);
+		}
+	}
+	
+}
